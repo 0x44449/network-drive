d582c7eca0e8873c09219b235135ea7e8d11a480
0x44449 <0x44449@gmail.com>
2022-05-14 21:14:00 +0900

    add spring template ngserver

---
+++ server/ngserver/src/main/java/com/ngserver/NgserverApplication.java
@@ -0,0 +1,13 @@
+package com.ngserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgserverApplication.class, args);
+	}
+
+}
---
+++ server/ngserver/src/test/java/com/ngserver/NgserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ngserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
