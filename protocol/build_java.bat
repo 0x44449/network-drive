affceacbe989746d862c4b74846f73f5baefbc1f
zina <0x44449@gmail.com>
2022-05-16 12:46:58 +0900

    direct build - proto java code

--- protocol/build_java.bat
+++ protocol/build_java.bat
@@ -1,4 +1,4 @@
-protobuf-3.20.1\bin\protoc --plugin=protoc-gen-grpc-java="./protobuf-3.20.1/java/protoc-gen-grpc-java-1.46.0-windows-x86_64.exe" --grpc-java_out="./java" --proto_path="./definitions" "./definitions/DriveProcedure.proto"
-protobuf-3.20.1\bin\protoc -I="./definitions" --java_out="./java" "./definitions/DriveProcedure.proto"
-protobuf-3.20.1\bin\protoc -I="./definitions/request" --java_out="./java" "./definitions/request/Model.proto"
-protobuf-3.20.1\bin\protoc -I="./definitions/response" --java_out="./java" "./definitions/response/Model.proto"
+protobuf-3.20.1\bin\protoc --plugin=protoc-gen-grpc-java="./protobuf-3.20.1/java/protoc-gen-grpc-java-1.46.0-windows-x86_64.exe" --grpc-java_out="../server/ngserver/src/main/java" --proto_path="./definitions" "./definitions/DriveProcedure.proto"
+protobuf-3.20.1\bin\protoc -I="./definitions" --java_out="../server/ngserver/src/main/java" "./definitions/DriveProcedure.proto"
+protobuf-3.20.1\bin\protoc -I="./definitions/request" --java_out="../server/ngserver/src/main/java" "./definitions/request/Model.proto"
+protobuf-3.20.1\bin\protoc -I="./definitions/response" --java_out="../server/ngserver/src/main/java" "./definitions/response/Model.proto"
